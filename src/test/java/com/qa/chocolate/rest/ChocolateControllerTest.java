55791e548bca1b62b08070118c81f93a99b0d0f3
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-02-04 18:01:32 +0000

    minor changes to test format2

--- src/test/java/com/qa/chocolate/rest/ChocolateControllerTest.java
+++ src/test/java/com/qa/chocolate/rest/ChocolateControllerTest.java
@@ -1,8 +1,11 @@
 package com.qa.chocolate.rest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,41 +25,63 @@ import com.qa.chocolate.domain.Chocolate;
 
 @SpringBootTest
 @AutoConfigureMockMvc // acts like your postman - makes the test requests
-@Sql(scripts = {"classpath:chocolate-schema.sql","classpath:chocolate-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)//pre-populate the database
+@Sql(scripts = { "classpath:chocolate-schema.sql",
+		"classpath:chocolate-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD) // pre-populate the
+																									// database
 @ActiveProfiles("test")
 public class ChocolateControllerTest {
-	
+
 	@Autowired
-	private MockMvc mock;//TO MOCK THE REQUESTS
-	
+	private MockMvc mock;// TO MOCK THE REQUESTS
+
 	@Autowired
-	private ObjectMapper map;//INTERPRETS JSON
-	
+	private ObjectMapper map;// INTERPRETS JSON
+
 	@Test
-	void testCreateCont() throws Exception {
-		//----request
-		//CREATE A CHOCOLATE
+	void createTest() throws Exception {
+		// ----request
+		// CREATE A CHOCOLATE
 		Chocolate newC = new Chocolate("AERO", "Cadbury", "Milk", 89, "Bubbles", 50);
-		//CONVERT INTO JSON string
+		// CONVERT INTO JSON string
 		String newCJSON = this.map.writeValueAsString(newC);
-		//Build the mock request
+		// Build the mock request
 		RequestBuilder mockRequest = post("/createChoco").contentType(MediaType.APPLICATION_JSON).content(newCJSON);
-		
-		///-----response
-		Chocolate savedC = new Chocolate(2L,"AERO", "Cadbury", "Milk", 89, "Bubbles", 50);//already inserted one record on line 21
-		//convert to JSON
+
+		/// -----response
+		Chocolate savedC = new Chocolate(2L, "AERO", "Cadbury", "Milk", 89, "Bubbles", 50);// already inserted one
+																							// record on line 21
+		// convert to JSON
 		String savedCJSON = this.map.writeValueAsString(savedC);
-		
-		//---test response (status + body)
-		//test status = 201-CREATED
+
+		// ---test response (status + body)
+		// test status = 201-CREATED
 		ResultMatcher matchStatus = status().isCreated();
-		//test response body
+		// test response body
 		ResultMatcher matchBody = content().json(savedCJSON);
-		
-		//perfom the test
+
+		// perfom the test
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchBody);
-		
-		
+
 	}
-	
+
+	@Test
+	void readTest() throws Exception {
+		Chocolate readC = new Chocolate(1L, "Twirl", "Cadbury", "Milk", 103, "Crumbly", 20);
+		List<Chocolate> allChoco = List.of(readC);
+		String readChocoJSON = this.map.writeValueAsString(allChoco);
+
+		RequestBuilder mockReq = get("/getChoco");
+
+		ResultMatcher status = status().isOk();
+		ResultMatcher body = content().json(readChocoJSON);
+
+		this.mock.perform(mockReq).andExpect(status).andExpect(body);
+
+	}
+
+	@Test
+	void updateTest() throws Exception {
+
+	}
+
 }
